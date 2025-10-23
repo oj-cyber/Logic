7e8ac73fab23dcca4ee97555acef2b290c60dce5
ojpaak169@gmail.com <ojpaak169@gmail.com>
2025-10-23 12:16:33 +0300

    Commit 2
    In the new version on line 3,6,9 and 15,Ii added a new salary variable of string datatype

--- Super/src/Nurse.java
+++ Super/src/Nurse.java
@@ -1,13 +1,18 @@
 public class Nurse extends Department{
     private final String shift;
+    String Salary;
 
-    public Nurse(String name, int deprtmentId, String department, String shift) {
+
+    public Nurse(String name, int deprtmentId, String department, String shift, String salary) {
         super(name, deprtmentId, department);
         this.shift = shift;
+        Salary = salary;
     }
+
     public void displayInfo() {
         System.out.println("ID: " + super.deprtmentId + " , Name: " + name);
         System.out.println("Dept: " + department + " ,Shift: " + shift);
+        System.out.println("salary:"+"Ugx"+Salary);
     }
 
 }
